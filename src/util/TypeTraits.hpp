a8239c8a775c3e38bf29886355e19119945d4e7d
std::move() <102883015+glibcxx@users.noreply.github.com>
2025-09-01 23:41:44 +0800

    fix: wrong result of type traits "is_virtual_thunk"

--- src/util/TypeTraits.hpp
+++ src/util/TypeTraits.hpp
@@ -2,23 +2,15 @@
 
 #include <string_view>
 #include <type_traits>
+#include "DecoratedName.hpp"
 
 namespace util::type_traits {
-
-    namespace detail {
-        template <auto ptr>
-        consteval auto is_virtual_thunk() {
-            constexpr std::string_view name{__FUNCDNAME__};
-            constexpr auto             begin = name.find("@$") + 3;
-            return name.substr(begin, 4) == "??_9";
-        }
-    } // namespace detail
 
     template <typename T>
     constexpr bool is_func_ptr = std::is_pointer_v<T> && std::is_function_v<std::remove_pointer_t<T>>
                               || std::is_member_function_pointer_v<T>;
 
     template <auto ptr>
-    constexpr bool is_virtual_thunk = detail::is_virtual_thunk<ptr>();
+    constexpr bool is_virtual_thunk = ::util::Decorator<ptr, true>::value.view().starts_with("??_9");
 
 } // namespace util::type_traits
