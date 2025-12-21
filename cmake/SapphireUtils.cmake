2461ed369cb0e96d790d3b9ff8c930c4d7340eb8
std::move() <102883015+glibcxx@users.noreply.github.com>
2025-12-21 13:30:40 +0800

    generate pdb in Release mode

--- cmake/SapphireUtils.cmake
+++ cmake/SapphireUtils.cmake
@@ -26,3 +26,18 @@ function(sapphire_add_install_target target_name)
         message(FATAL_ERROR "Neither COMPONENT nor DEPENDS specified for target: ${target_name}")
     endif()
 endfunction()
+
+function(sapphire_enable_release_pdb)
+    foreach(target_name IN LISTS ARGN)
+        if(TARGET ${target_name})
+            target_compile_options(${target_name} PRIVATE
+                "$<$<CONFIG:Release>:/Zi>"
+            )
+            target_link_options(${target_name} PRIVATE
+                "$<$<CONFIG:Release>:/DEBUG;/OPT:REF;/OPT:ICF>"
+            )
+        else()
+            message(AUTHOR_WARNING "sapphire_enable_release_pdb: Target '${target_name}' does not exist.")
+        endif()
+    endforeach()
+endfunction()
